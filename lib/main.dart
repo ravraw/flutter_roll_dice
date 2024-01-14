8c2424b47267d628a6891990501b6a84f796f60a
“ravinrawat” <ravinrawat.dev@gmail.com>
2024-01-14 15:12:31 -0700

    flutter_roll_dice: extracted Widget tree to MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        // backgroundColor: Color.fromARGB(255, 229, 102, 93),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 232, 168, 163),
-                  Color.fromARGB(255, 147, 23, 15)
-                ]),
-          ),
-          child: const Center(
-            child: Text(
-              "Flutter Roll Dice",
-              style: TextStyle(
-                fontSize: 24,
-                color: Color.fromARGB(255, 255, 255, 253),
-              ),
-            ),
+          // backgroundColor: Color.fromARGB(255, 229, 102, 93),
+          body: MyApp()),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 232, 168, 163),
+              Color.fromARGB(255, 147, 23, 15)
+            ]),
+      ),
+      child: const Center(
+        child: Text(
+          "Flutter Roll Dice",
+          style: TextStyle(
+            fontSize: 24,
+            color: Color.fromARGB(255, 255, 255, 253),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
