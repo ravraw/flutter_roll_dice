cc9372e906897e7706058c66c60f78f0546e34fb
“ravinrawat” <ravinrawat.dev@gmail.com>
2024-01-14 16:14:11 -0700

    flutter_roll_dice: added colors as parameters to GradientContainer

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -3,21 +3,22 @@ import 'package:flutter_roll_dice/styled_text.dart';
 
 const Alignment startAlignment = Alignment.topLeft;
 const Alignment endAlignment = Alignment.bottomLeft;
-const Color colorOne = Color.fromARGB(255, 232, 168, 163);
-const Color colorTwo = Color.fromARGB(255, 147, 23, 15);
 
 class GradiantContainer extends StatelessWidget {
-  const GradiantContainer({super.key});
+  const GradiantContainer(this.colorOne, this.colorTwo, {super.key});
+
+  final Color colorOne;
+  final Color colorTwo;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
               begin: startAlignment,
               end: endAlignment,
               colors: [colorOne, colorTwo]),
         ),
-        child: StyledText("Flutter Roll Dice"));
+        child: const StyledText("Flutter Roll Dice"));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_roll_dice/gradient_container.dart';
 
+const Color colorOne = Color.fromARGB(255, 232, 168, 163);
+const Color colorTwo = Color.fromARGB(255, 147, 23, 15);
+
 void main() {
   runApp(
     const MaterialApp(
@@ -14,6 +17,6 @@ class FlutterRollDice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GradiantContainer();
+    return const GradiantContainer(colorOne, colorTwo);
   }
 }
