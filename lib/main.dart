4758b01e361c59c94e2f2d613c35692a48e87461
“ravinrawat” <ravinrawat.dev@gmail.com>
2024-01-14 15:26:33 -0700

    flutter_roll_dice: Extracted to GradientContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_roll_dice/GradientContainer.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(
-          // backgroundColor: Color.fromARGB(255, 229, 102, 93),
-          body: FlutterRollDice()),
+      home: Scaffold(body: FlutterRollDice()),
     ),
   );
 }
@@ -15,25 +14,6 @@ class FlutterRollDice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color.fromARGB(255, 232, 168, 163),
-              Color.fromARGB(255, 147, 23, 15)
-            ]),
-      ),
-      child: const Center(
-        child: Text(
-          "Flutter Roll Dice",
-          style: TextStyle(
-            fontSize: 24,
-            color: Color.fromARGB(255, 255, 255, 253),
-          ),
-        ),
-      ),
-    );
+    return const GradiantContainer();
   }
 }
