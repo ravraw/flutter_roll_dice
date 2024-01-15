0bc15d3b6ba5478ec9e6b05f8cf26e4021950a5e
“ravinrawat” <ravinrawat.dev@gmail.com>
2024-01-14 18:52:29 -0700

    flutter_roll_dice: removed debug banner

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -28,12 +28,13 @@ class _DiceRollerState extends State<DiceRoller> {
       children: [
         Image.asset('assets/images/dice-$currentDiceRoll.png', width: 200),
         const SizedBox(height: 20),
-        TextButton(
+        ElevatedButton(
           onPressed: rollDice,
           style: TextButton.styleFrom(
-            padding: const EdgeInsets.all(20),
-            foregroundColor: Colors.white,
-            textStyle: const TextStyle(fontSize: 28),
+            padding: const EdgeInsets.all(10),
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
           ),
           child: const Text('Roll Dice'),
         )
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_roll_dice/flutter_roll_dice.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: FlutterRollDice(),
       ),
